4d862bcf9b3e6afe9aaddd58538b4ea819b7ae61
ctgmovies198 <ctgmovies198@gmail.com>
2025-07-08 15:52:03 +0600

    Update Redeem.py

--- plugins/Extra/Redeem.py
+++ plugins/Extra/Redeem.py
@@ -50,10 +50,10 @@ Aᴍᴏᴜɴᴛ:</b> {num_codes}
             [
                 [
                     InlineKeyboardButton(
-                        "♻️ Redeem Here ♻️", url="http://t.me/NehaTestBot"
+                        "♻️ Redeem Here ♻️", url="http://t.me/MovieChannel_1Bot"
                     )
                 ],
-                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/IM_JISSHU")],
+                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/YABOTZ_BOT")],
             ]
         )
 
