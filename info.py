ddf44d9f787cb48c6c34cddbcb4b060a23c588b4
ctgmovies198 <ctgmovies198@gmail.com>
2025-07-08 16:00:00 +0600

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "https://t.me/YABOTZ_BOT")
 MOVIE_GROUP_LINK = environ.get("MOVIE_GROUP_LINK", "https://t.me/MovieChannel_BD")
 
 # Verification
-IS_VERIFY = is_enabled("IS_VERIFY", Trur)
+IS_VERIFY = is_enabled("IS_VERIFY", True)
 # ---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/yamovieflixdownload/13")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/yamovieflixdownload/13")
